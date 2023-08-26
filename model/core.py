Fix exploding gradient by clamping probability
- Probability is now lower bounded to avoid exploding gradient when taking the logarithm
- Also added some print statements to indicate progression through stages of training

model/core.py
@@ -7,6 +7,12 @@ from gymnasium.spaces import Tuple, Box, Discrete
 
 from envs import BaseArmEnv
 
+MIN_PROB = torch.tensor(1e-12) 
+
+# lower bound probability to avoid issues from computing the log of 0
+def clamp_prob(inp):
+    return torch.clamp(inp, min = MIN_PROB)
+
 def KNN(data, labels, new_point, k):
     dists = np.linalg.norm(data - new_point, axis = 1)
     top_idxs = np.argsort(dists)[:k]
@@ -85,7 +91,7 @@ class SquashedGaussianMLPActor(nn.Module):
 
         with torch.no_grad():
             r = 0.5 if deterministic else torch.rand_like(p)
-            discrete_action = (r < p).int() # sample 1 with probability p
+            discrete_action = r < p # sample 1 with probability p
 
         if with_logprob:
             # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
@@ -95,12 +101,12 @@ class SquashedGaussianMLPActor(nn.Module):
             # Try deriving it yourself as a (very difficult) exercise. :)
             logp_pi = continuous_dist.log_prob(continuous_action).sum(axis = -1)
             logp_pi -= 2 * (np.log(2) - continuous_action - F.softplus(-2 * continuous_action)).sum(axis = 1)
-            logp_discrete = discrete_action * torch.log(p) + (1 - discrete_action) * torch.log(1 - p)
+            logp_discrete = torch.where(discrete_action, torch.log(clamp_prob(p)), torch.log(clamp_prob(1 - p)))
             logp_pi += torch.squeeze(logp_discrete, dim = -1)
         else:
             logp_pi = None
         continuous_action = self.act_limit * torch.tanh(continuous_action)
-        return torch.cat((continuous_action, discrete_action), dim = -1), logp_pi
+        return torch.cat((continuous_action, discrete_action.int()), dim = -1), logp_pi
 
 
 class MLPQFunction(nn.Module):

model/mac.py
@@ -12,8 +12,7 @@ from tqdm import tqdm
 
 from model.core import *
 
-MIN_COUNT = 40
-NUM_KERNEL_PCA_COMPONENTS = 10
+NUM_KERNEL_PCA_COMPONENTS = 10 # size of latent dimension
 NUM_NEIGHBORS = 9 # k for kNN
 
 class MAC:
@@ -207,7 +206,7 @@ class MAC:
         self.pi_optimizer.zero_grad()
         for subcontroller_index in range(self.num_subcontrollers):
             # give more weight to envs that use the subcontroller more often; this sampling scheme is equivalent (in
-            # probability) to directly drawing from all environment steps that use the subcontroller (across tasks)
+            # probability) to directly drawing from all environment steps that use the subcontroller (across all tasks)
             env_probs = sum_to_one([buffer.num_steps(subcontroller_index) for buffer in self.replay_buffers])
             # number of samples to take from each environment
             batch_sizes = np.random.multinomial(self.batch_size, pvals = env_probs)
@@ -281,9 +280,6 @@ class MAC:
         # run clustering algorithm
         HAC = AgglomerativeClustering(n_clusters = self.num_subcontrollers, linkage = 'ward', compute_full_tree = False)
         assignments = HAC.fit_predict(transformed_data)
-        _, counts = np.unique(assignments, return_counts = True)
-        assert np.all(counts >= MIN_COUNT), \
-            f'Every cluster must have at least {MIN_COUNT} samples to avoid issues with GD on too few samples'
         # store transformed data to run KNN in future timesteps
         self.knn_points[env_index][:self.update_after] = transformed_data
         # store assigned subcontrollers
@@ -299,11 +295,15 @@ class MAC:
         observations = [self.envs[i].reset()[0] for i in range(self.num_envs)]
 
         for t in tqdm(range(total_steps)):
+            if t == self.start_steps:
+                print('Finished exploration')
+            elif t == self.update_after:
+                print('Started assigning subcontrollers and backpropagating')
             # Environment interactions for all tasks
             for env_index, env in enumerate(self.envs):
                 # Until start_steps have elapsed, randomly sample actions from a uniform
                 # distribution for better exploration. Afterwards, use the learned policy.
-                if t > self.start_steps:
+                if t >= self.start_steps:
                     action, subcontroller_index = self.get_action(observations[env_index], env_index)
                 else:
                     action = env.action_space.sample()