Fix throw reward scaling and restructure catch reward
- Catch reward was restructured to be like throw rewards
- ThrowEnv termination penalties are way bigger now to balance out the new much larger distance based rewards and punishments
- Broke reward function API compatibility (think this is more natural because they're so disparate)

envs/BaseArmEnv.py
@@ -100,7 +100,7 @@ class BaseArmEnv(MujocoEnv):
 
         self._launch_point_pos = self.model.body('launch_point').pos
         self._target_pos = self.model.body('target').pos
-        self.storage_point = np.array([1.2, 0., 0.])
+        self._storage_point = np.array([1.2, 0., 0.])
         self.ball_radius = self.model.geom('ball_geom').size[0]
 
         self.previous_obs = None
@@ -312,10 +312,16 @@ class BaseArmEnv(MujocoEnv):
         v0 = v_avg + 0.5 * g * t # v_avg = v0 - 0.5 * g * t -> v0 = v_avg + 0.5 * g * t
         return delta_x / t, v0
 
+    # hides either the target or launch point geometries
     def hide(self, body_name):
         assert body_name in ['target', 'launch_point']
-        self.model.body(body_name).pos = self.storage_point
+        self.model.body(body_name).pos = self._storage_point
         self.model.geom(f"{body_name}_geom").rgba = INVISIBLE
+
+    def scaled_reward(self, changed_fist, ball_was_within_reach):
+        is_catch_env = (self.__class__.__name__ == 'CatchEnv')
+        raw_reward = self.reward(changed_fist, ball_was_within_reach) if is_catch_env else self.reward()
+        return self._reward_weight * raw_reward
 
     def step(self, action):
         bools = [self.ball_in_hand, self.closed_fist]
@@ -323,12 +329,13 @@ class BaseArmEnv(MujocoEnv):
         self.t += 1
         control, close_fist = action
         changed_fist = (self.closed_fist != close_fist)
+        ball_was_within_reach = self.ball_within_reach # only useful for CatchEnv
         self.do_simulation(control, self.frame_skip)
 
         self.handle_fist(close_fist)
         self.handle_fist_appearance()
         observation = self._get_obs()
-        rewards = self._reward_weight * self.reward(changed_fist)
+        rewards = self.scaled_reward(changed_fist, ball_was_within_reach)
         ctrl_cost, changed_fist_cost, total_cost = self.control_cost(control, changed_fist)
         net_reward = rewards - total_cost
         terminated = self.should_terminate() or self.invalid_position() or self.stuck(observation)

envs/ThrowEnv.py
@@ -65,9 +65,7 @@ class ThrowEnv(BaseArmEnv):
         return self._get_obs()
 
     # reward is only received at the perigee (i.e. right before the episode terminates)
-    # N.B. there are definitely some edge cases where these conditions overlap: these cases
-    # are ignored for simplicity ebcause they should be sufficiently rare
-    def reward(self, changed_fist):
+    def reward(self):
         # COMPONENT 1: primary reward that is based on the closest the ball gets to the target i.e. how far it is at the perigee
         if self.released and (self.at_perigee or self.invalid_position()):
             threshold = 0.125
@@ -89,11 +87,8 @@ class ThrowEnv(BaseArmEnv):
             up_and_right = (self.ball_vel[0] > 0) and (self.ball_vel[2] > 0)
             # cap velocity reward because there's diminishing returns, and this is an auxiliary reward
             # to help the arm find the true, primary reward (which is based on distance to target)
-            return np.clip(np.linalg.norm(self.ball_vel), 0, MAX_VEL) / 30 if up_and_right else 0
-        # COMPONENT 3: smaller penalty to prevent the robot from just ending the episode by hitting the floor
-        if self.invalid_position() and not self.released:
-            return -2
-        # COMPONENT 4: big penalty to disincentivize holding onto the ball
-        if self.t == self.MAX_EPISODE_LENGTH:
-            return -10
+            return 10 * np.clip(np.linalg.norm(self.ball_vel), 0, MAX_VEL) if up_and_right else 0
+        # COMPONENT 3: penalty to prevent the robot from ending the episode by just hitting the floor or holding onto the ball
+        if (self.invalid_position() and not self.released) or self.t == self.MAX_EPISODE_LENGTH:
+            return -8000
         return 0 # default is no reward